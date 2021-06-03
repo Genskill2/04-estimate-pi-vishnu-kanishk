f61444cf8e8165049988c32cd29a4a89475b36db
Vishnu Kanishk <68165401+vishnu-kanishk@users.noreply.github.com>
2021-06-03 18:41:12 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,6 +38,22 @@ int main(void) {
     }
   }
 }
+float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
+	x=frandom();
+	y=frandom();
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
+	}
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
+}
 
 
 
